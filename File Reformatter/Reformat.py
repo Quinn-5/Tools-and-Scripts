f5ee3fc3eaaeb756ed07bbb6d5ffff36d6dd0832
Quinlan Duval <quinlan.duval@comcast.net>
2019-01-03 14:12:25 -0500

    Create Reformat.py

---
+++ File Reformatter/Reformat.py
@@ -0,0 +1,19 @@
+with open('primes1.txt') as f:
+    Primes = f.readlines()
+    # Primes = [x.strip() for x in Primes]
+print(Primes)
+
+length = Primes.__len__()
+
+for i in range(0, length):
+    if i+1 == length:
+        break
+    print(str(i) + ' out of ' + str(length))
+    if Primes[i] == '\n':
+        del Primes[i]
+    length = Primes.__len__()
+with open('primes.txt', 'a') as f:
+    for item in Primes:
+        f.write(item)
+
+print(Primes)
