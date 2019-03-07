014d0930a012eb4ba9c3b19bc1dab46cab4ca30e
Quinn <quinlan.duval@comcast.net>
2019-03-07 11:49:57 -0500

    Minor Improvements
    removed some unnecessary lines, and allow the input/output files to be more easily changed

--- File Reformatter/Reformat.py
+++ File Reformatter/Reformat.py
@@ -1,64 +1,46 @@
-def  main(): 
-    replacement = str.maketrans(' ', '\n')
-    with open('primes1.txt') as f:
-        Primes = f.readlines()
-    f.close
+src = 'primes1.txt' # str(input("What is the path to the input file?\n"))
+dest = 'primes.txt' # str(input("What is the path to the output file?\n"))
 
-    print(Primes)
+replacement = str.maketrans(' ', '\n')
+with open(src) as f:
+    start = f.readlines()
+f.close
 
-    length = Primes.__len__()
+length = start.__len__()
 
-    temp = open('primes.txt', 'w')
-    temp.write('')
-    temp.close
-    
-    i = 0
-    file = open('primes.txt', 'a')
+temp = open(dest, 'w')
+temp.write('')
+temp.close
 
-    for j in range (0, length):
-            print(str(j) + ' out of ' + str(length))
-            tempTable = []
-            for k in Primes[j]:
-                if k == ' ':
-                    tempTable.append(k.translate(replacement))
-                else:
-                    tempTable.append(k)
-            # Primes[j] = tempTable
-            with open('primes.txt', 'a') as f:
-                for item in tempTable:
-                    f.write(item)
-    
- 
-
-    # This section opens the file and deletes all empty lines 
-    with open('primes.txt') as f:
-        Primes = f.readlines()
-
-    print(Primes)
-   
-    length = Primes.__len__()
-    i=0
-    while i < length:
-        print(str(i) + ' out of ' + str(length))
-
-        if Primes[i] == '\n':
-            del Primes[i]
+for j in range (0, length):
+    print(str(j) + ' out of ' + str(length))
+    tempTable = []
+    for k in start[j]:
+        if k == ' ':
+            tempTable.append(k.translate(replacement))
         else:
-            i += 1
-        if i+1 == length:
-            break
-        length = Primes.__len__()
+            tempTable.append(k)
+    with open(dest, 'a') as f:
+        for item in tempTable:
+            f.write(item)
 
+# This section opens the file and deletes all empty lines 
+with open(dest) as f:
+    start = f.readlines()
 
-    
-    with open('primes.txt', 'a') as f:
-        for items in Primes:
-            f.write(items)
+length = start.__len__()
+i=0
+while i < length:
+    print(str(i) + ' out of ' + str(length))
 
+    if start[i] == '\n':
+        del start[i]
+    else:
+        i += 1
+    if i+1 == length:
+        break
+    length = start.__len__()
 
-
-
-
-    print(Primes)
-
-main()
+with open(dest, 'a') as f:
+    for items in start:
+        f.write(items)
